c3dca1820853db9ad8ad1eb8371597c0e38798ae
Graham Gilchrist <mail@grahamgilchrist.com>
2015-05-04 19:16:43 +0100

    Update macos.sh

--- macos.sh
+++ macos.sh
@@ -1,6 +1,8 @@
 # Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install brew cask
+brew install caskroom/cask/brew-cask
 
 # Install git, hub and git completion
 brew install git hub git-extras bash-completion
