f3acb84078b401b2c0ca74c57732294fc5c563bf
Graham Gilchrist <mail@grahamgilchrist.com>
2015-05-05 12:33:54 +0100

    Check brew not installed before trying to install

--- macos.sh
+++ macos.sh
@@ -1,5 +1,8 @@
 # Install homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+echo "========== install homebrew"
+if ! hash brew 2>/dev/null; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 # Install brew cask
 brew install caskroom/cask/brew-cask
