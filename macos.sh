93becaa77fc4a7c2c8f674e718c2ca7c1721dd89
Graham Gilchrist <mail@grahamgilchrist.com>
2015-05-05 12:42:33 +0100

    Add brew and cask packages

--- macos.sh
+++ macos.sh
@@ -1,11 +1,37 @@
 # Install homebrew
+# ========
 echo "========== install homebrew"
 if ! hash brew 2>/dev/null; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Install brew cask
-brew install caskroom/cask/brew-cask
+brew install bash
+brew install bash-completion
+brew install wget
 
-# Install git, hub and git completion
-brew install git hub git-extras bash-completion
+brew install git
+brew install git-extras
+brew install hub
+
+brew install python
+brew install python3
+
+brew install boot2docker
+brew install docker-compose
+
+# Install brew cask
+# brew install caskroom/cask/brew-cask
+brew tap caskroom/cask
+brew install brew-cask
+
+brew cask install alfred
+brew cask install dropbox
+brew cask install firefox
+brew cask install handbrake
+brew cask install iterm2
+brew cask install licecap
+brew cask install postgres
+brew cask install slack
+brew cask install spotify
+brew cask install the-unarchiver
+brew cask install vlc
